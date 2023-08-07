331bbe6a61ac39694cbb62644c90afe042a27d55
thurrsense <arthur.khasanov20@gmail.com>
2023-08-07 22:32:43 +0300

    Add: solve Lists

--- lists/lists.py
+++ lists/lists.py
@@ -1,23 +1,44 @@
 class ListExercise:
     @staticmethod
-    def replace(input_list: list[int]) -> list[int]:
-        """
-        Заменить все положительные элементы целочисленного списка на максимальное значение
-        элементов списка.
+    def max_val(input_list: list[int]) -> int:
+        max = 0
+        for el in input_list:
+            if max < el:
+                max = el
+        return max
 
-        :param input_list: Исходный список
-        :return: Список с замененными элементами
-        """
-        pass
+    @staticmethod
+    def replace(input_list: list[int]) -> list[int]:
+        if not input_list:
+            return input_list
+
+        max_input = ListExercise.max_val(input_list)
+
+        for index in range(len(input_list)):
+            if input_list[index] > 0:
+                input_list[index] = max_input
+
+        return input_list
+
+    @staticmethod
+    def binary_search(input_list: list[int], left: int, right: int, query: int) -> int:
+        if left <= right:
+
+            mid = (left + right) // 2
+            value = input_list[mid]
+
+            if value == query:
+                return mid
+            elif value > query:
+                return ListExercise.binary_search(input_list, left, mid - 1, query)
+            else:
+                return ListExercise.binary_search(input_list, mid + 1, right, query)
+
+        return -1
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
-        """
-        Реализовать двоичный поиск
-        Функция должна возвращать индекс элемента
+        left = 0
+        right = len(input_list) - 1
 
-        :param input_list: Исходный список
-        :param query: Искомый элемент
-        :return: Номер элемента
-        """
-        pass
+        return ListExercise.binary_search(input_list, left, right, query)
